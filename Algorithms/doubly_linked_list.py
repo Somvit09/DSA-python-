13f24593c78f8fb0a0d0f2cae5f330115d1f1fa5
Somvit09 <developer.09.sam@gmail.com>
2023-03-18 19:15:21 +0530

    edited

--- Algorithms/doubly_linked_list.py
+++ Algorithms/doubly_linked_list.py
@@ -9,6 +9,7 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
+    # Print
 
     # printing the linked list
     def PrintLinkedList(self):
@@ -17,7 +18,7 @@ class DoublyLinkedList:
             return
         n = self.head
         while n is not None:
-            print(n.data, end="-->")
+            print(n.data, end="<-->")
             n = n.next
 
     # printing linked list from backwords
@@ -29,9 +30,11 @@ class DoublyLinkedList:
         while n.next is not None:
             n = n.next
         while n is not None:
-            print(n.data, end="-->")
+            print(n.data, end="<-->")
             n = n.prev
         return 
+    
+    # Inserting
     
     # inserting node into an empty linkedlist
     def InsertIntoEmptyLL(self, data):
@@ -103,6 +106,77 @@ class DoublyLinkedList:
             self.head = new_node
         n.prev = new_node
         return 
+    
+    # Deleting
+
+    # delete from begin
+    def DeleteFromBegin(self):
+        if self.head is None:
+            print("linked list is empty")
+            return
+        if self.head.next is None:
+            self.head = None
+            print("after deleting the one and only or first node")
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        return
+    
+    # delete from end
+    def DeleteFromEnd(self):
+        if self.head is None:
+            print("linked list is epmty")
+            return 
+        if self.head.next is None:
+            self.head = None
+            print("after deleting the one and only or first node")
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.prev.next = None
+        return
+    
+    # delete by a specific value
+    def DeleteByValue(self, x):
+        if self.head is None:
+            print("linked list is empty")
+            return
+        # if head is the only node present in the linked list then
+        if self.head.next is None:
+            if self.head.data == x:
+                self.head = None
+            else:
+                print(f"node {x} is not present")
+            return
+        # if there are other nodes except head node then
+        else:
+            # if x is the head node to be deleted 
+            if self.head.data == x:
+                self.head = self.head.next
+                self.head.prev = None
+                print("node deleted")
+                return
+            # if the x node is somewhere present in the linked list
+            n = self.head
+            while n.next is not None:
+                if n.data == x:
+                    break
+                n = n.next
+            # if the x node is the last node 
+            if n.next is None:
+                if n.data == x:
+                    n.prev.next = None
+                else:
+                    print(f"node {x} is not present in the linked list.")
+                return
+            # if it niether the last node or the first node. except it it present at n position then
+            n.prev.next = n.next
+            n.next.prev = n.prev
+        return
+            
+
+
 
 
 
@@ -111,4 +185,9 @@ sol = DoublyLinkedList()
 sol.InsertIntoEmptyLL(1)
 sol.InsertAtBegin(5)
 sol.InsertFromEnd(6)
+sol.InsertAfterANode(2, 1)
+sol.InsertBeforeANode(4, 1)
+sol.DeleteFromBegin()
+sol.DeleteFromEnd()
+sol.DeleteByValue(1)
 sol.PrintLinkedList()
